2db8239fbce8754d8a565c1d3277a6cc4285c14e
YoC00lig <kulig.j@vp.pl>
2023-01-06 15:57:42 +0100

    small changes

--- src/main/java/org/solver/Solution.java
+++ src/main/java/org/solver/Solution.java
@@ -12,7 +12,7 @@ public class Solution {
 
 
     public Solution(int number_of_elements) {
-        this.gauss_integrator= new IterativeLegendreGaussIntegrator(3, Math.pow(0.1,5), Math.pow(0.1,5));
+        this.gauss_integrator= new IterativeLegendreGaussIntegrator(2, Math.pow(0.1,5), Math.pow(0.1,5));
         this.n = number_of_elements;
         this.h = domain / n;
     }
@@ -20,10 +20,10 @@ public class Solution {
     private static double K(double x) {
         return (0<= x && x<=1) ? 1.0 : 2.0;
     }
-
+    // e functions
     private double e(double x, int k){
         if (Math.max(h * (k-1), 0) <= x && x <= h*k) return (x - h*(k-1))/h;
-        else if (h * k <= x && x <= Math.min(h*(k+1),2)) return (h*(k+1) - x)/h;
+        else if (h * k < x && x <= Math.min(h*(k+1),2)) return (h*(k+1) - x)/h;
         else return 0;
     }
 
@@ -40,7 +40,7 @@ public class Solution {
                 if (col < size && Math.abs(row-col) <= 1) B.setEntry(row, col, BUV(row, col));
             }
         }
-        RealVector L = LV(size);
+        RealVector L = LV(size); // L(v)
         return new LUDecomposition(B).getSolver().solve(L);
     }
 
@@ -55,11 +55,11 @@ public class Solution {
         double to = Math.min(get_from_to(i, i*h)[1], get_from_to(j, j*h)[1]);
         return new double[]{from, to};
     }
-
+    // B(u,v) i L(v)
     private double BUV( int i, int j) {
         double[] range =  get_integrate_range(i, j);
-        double integral = gauss_integrator.integrate(Integer.MAX_VALUE, x ->  K(x) * ePrim(x,i) * ePrim(x,j), range[0], range[1]);
-        double second_value_to_subtract = (i < 2 && j < 2) ? K(0)*e(0,i)*e(0,j) : 0;
+        double integral = gauss_integrator.integrate(Integer.MAX_VALUE, x ->  K(x) * ePrim(x, i) * ePrim(x, j), range[0], range[1]);
+        double second_value_to_subtract = (i < 2 && j <= 2) ? K(0)*e(0,i)*e(0,j) : 0;
         return integral - second_value_to_subtract;
     }
 
